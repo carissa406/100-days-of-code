a8f6f0542db4a3e93045049a02f5f4725f00c343
Carissa Hicks <38417097+carissa406@users.noreply.github.com>
2023-10-19 19:41:03 -0500

    update

---
+++ Day 22/paddle.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+MOVE_DISTANCE = 20
+
+class Paddle(Turtle):
+
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.penup()
+        self.shapesize(stretch_wid=5, stretch_len=1, outline=None)
+        self.goto(position)
+
+    def up(self):
+        new_y = self.ycor() + MOVE_DISTANCE
+        self.goto(self.xcor(), new_y)
+
+    def down(self):
+        new_y = self.ycor() - MOVE_DISTANCE
+        self.goto(self.xcor(), new_y)
