3b77aa08ca28c6c94cb805b726f7774768bd3949
Carissa Hicks <38417097+carissa406@users.noreply.github.com>
2024-01-08 11:32:49 -0800

    update

--- Day 40/flightmanager/6e625d78157c9282061723a04baf6827-1d4d8867451f19c828fe09accf6920dcfde8e6a3/data_manager.py
+++ Day 40/flightmanager/6e625d78157c9282061723a04baf6827-1d4d8867451f19c828fe09accf6920dcfde8e6a3/data_manager.py
@@ -1,5 +1,12 @@
+import requests
+import os
+
+SHEET_ENDPOINT = os.getenv("FLIGHT_SHEET_ENDPOINT")
+USERNAME = os.getenv("EXERCISE_USERNAME")
+PASSWORD = os.getenv("EXERCISE_PASSWORD")
+
 def get_customer_emails(self):
-    customers_endpoint = SHEET_USERS_ENDPOINT
+    customers_endpoint = SHEET_ENDPOINT
     response = requests.get(customers_endpoint)
     data = response.json()
     self.customer_data = data["users"]
